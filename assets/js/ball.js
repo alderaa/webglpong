2dad4af1b3cb36a81d24683949104ca8f4d0e38c
alderaa <aaron_alder@byu.edu>
2015-04-13 11:31:39 -0600

    Socket branch almost working

--- assets/js/ball.js
+++ assets/js/ball.js
@@ -1,30 +1,29 @@
-getBalls = function(num){	
-	balls = [];
-	for(var i=0;i<num;i++){
-        var ball = new Physijs.SphereMesh(
-	        new THREE.SphereGeometry(10, 30, 30),
-	        new THREE.MeshLambertMaterial({color:new THREE.Color( 0xff0000 )}),
-	        1
-	    );
-	    ball.position.y = 250;
-		scene.add( ball );
-		ball.castShadow = true;
-		ball.receiveShadow = false;
-        ball.setLinearVelocity(new THREE.Vector3(Math.random()*100+100,Math.random()*100+100,Math.random()*100+100));
-        ball.addEventListener( 'collision', function( other_object, relative_velocity, relative_rotation, contact_normal) {
-		   	var audio = new Audio('pong.wav');
-			audio.play();
-		   	var incoming = new THREE.Vector3(1.02 * (-ball.getLinearVelocity().x), 1.02 * (-ball.getLinearVelocity().y), 1.02*(-ball.getLinearVelocity().z));
-		   	var newVel = incoming.reflect(contact_normal);
-		   	ball.setLinearVelocity(newVel);
-		});
-		balls.push(ball);
-    }
-    return balls;
+getBall = function(){	
+	
+    var ball = new Physijs.SphereMesh(
+        new THREE.SphereGeometry(10, 30, 30),
+        new THREE.MeshLambertMaterial({color:new THREE.Color( 0xff0000 )}),
+        1
+    );
+    ball.position.y = 250;
+	scene.add( ball );
+	ball.castShadow = true;
+	ball.receiveShadow = false;
+	
+    ball.addEventListener( 'collision', function( other_object, relative_velocity, relative_rotation, contact_normal) {
+	   	var audio = new Audio('pong.wav');
+		audio.play();
+	   	var incoming = new THREE.Vector3(1.02 * (-ball.getLinearVelocity().x), 1.02 * (-ball.getLinearVelocity().y), 1.02*(-ball.getLinearVelocity().z));
+	   	var newVel = incoming.reflect(contact_normal);
+	   	if(playerid=="one")
+				socket.emit('bounce', newVel);
+	});
+		
+    return ball;
 };
 
 resetBall = function(ball, direction){
-	ball.position.set(0,250,0);
-	ball.__dirtyPosition = true;
-	ball.setLinearVelocity(new THREE.Vector3(Math.random()*100+100, Math.random()*100+100, direction*(Math.random()*100+100)));
+	var newVel = new THREE.Vector3(Math.random()*100+100, Math.random()*100+100, direction*(Math.random()*100+100));
+	if(playerid=="one")
+		socket.emit('start', newVel);
 }
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
-
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var player1 = 0;
 app.set('views', __dirname+"/views");
 app.engine('html', require('ejs').renderFile);
 
@@ -13,7 +16,42 @@ app.get('/test', function (req, res) {
   res.render("phystest.html");
 })
 
-var server = app.listen(3015, function () {
+io.on('connection', function(socket){
+   if(!player1){
+   	 socket.emit('playerid','one');
+   	 player1=1;
+   }
+   else{
+   	 socket.emit('playerid','two');
+   	 player1=0;
+   }
+   socket.on('bounce', function(msg){
+    io.emit('bounce', msg);
+   });
+   socket.on('moved',function(data){
+   	socket.broadcast.emit('otherplayermove',data);
+   });
+   socket.on('ready',function(data){
+   	socket.broadcast.emit('ready',data);
+   });
+   socket.on('begin',function(data){
+   	io.emit('begin',data);
+   });
+   socket.on('start',function(data){
+   	io.emit('start',data);
+   });
+   socket.on('point1',function(data){
+   	io.emit('point1',data);
+   });
+   socket.on('point2',function(data){
+   	io.emit('point2',data);
+   });
+   
+   
+});
+
+
+var server = http.listen(3015, function () {
 
   var host = server.address().address
   var port = server.address().port
